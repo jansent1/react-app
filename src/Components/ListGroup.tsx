335d52f183dee49fb89b758aed861781070cc4fb
Teun Jansen <teunjansen91@gmail.com>
2024-03-28 12:26:45 +0100

    ListGroup Component v2.0

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
+  const items = ["Amsterdam", "Antwerpen", "Arnhem", "London", "Paris"];
+  // items = [];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Group:</h1>
+      {items.length === 0 ? <p>No cities found</p> : null}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
